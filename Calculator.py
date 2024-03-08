da4da006985a139411d166ec989981a998e649e5
Chelsea Garcia <chelseagarcia@Chelseas-MBP.attlocal.net>
2024-03-07 22:11:35 -0600

    Updated Image files

--- Calculator.py
+++ Calculator.py
@@ -4,12 +4,12 @@ import pygame
 
 class Calculator:
     def __init__(self):
-        self.dimensions = (608,889)
+        self.dimensions = (400,600)
         pygame.init()
         pygame.display.set_caption('Calculator')
         self.screen = pygame.display.set_mode(self.dimensions)
         self.clock = pygame.time.Clock()
-        self.img = pygame.image.load('cal/1.png')
+        self.img = pygame.image.load('Sprites/Calculator_sprites/Calculator.png')
         self.img = pygame.transform.scale(self.img, self.dimensions)
         # self.buttons = []
     
@@ -19,8 +19,8 @@ class Calculator:
 
             # for button in self.buttons:
             #     self.screen.blit(button.img,button.cord)
-            self.screen.blit(pygame.image.load('cal/2.png'),(195,683))
-            self.screen.blit(pygame.image.load('cal/3.png'),(50,160))
+            self.screen.blit(pygame.image.load('Sprites/Calculator_sprites/1_trans.png'),(0,10))
+            self.screen.blit(pygame.image.load('Sprites/Calculator_sprites/2_trans.png'),(0,10))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -28,25 +28,4 @@ class Calculator:
 
             pygame.display.update()
             self.clock.tick(60)
-
-# class Button:
-#     def __init__(self, imgPath, cord, value, screen):
-#         self.img = pygame.image.load(imgPath)
-#         self.img = pygame.transform.scale(self.img, (50,50))
-#         self.cord = cord
-#         self.value = value 
-#         self.screen = screen 
-    
-    
-#     def run(self):
-#         while True:
-#             self.screen.blit(self.img,self.cord)
-
-
-calc = Calculator()
-calc.run()
-
-# button0 = Button('Sprites/calculator_sprites/0.png', (100,100), 0, calc.screen)
-# calc.buttons.append(button0)
-# button1 = Button('Sprites/calculator_sprites/1.png', (150,150), 1, calc.screen)
-# calc.buttons.append(button1)
+Calculator().run()
